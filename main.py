39e523e3b88be64e070f2253da763a66a2c0777b
JAAR <joe.aaron.acosta@outlook.com>
2023-08-17 21:56:54 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 08/17/2023
+>>> Practicing Fundamentals Program 19
+>>> Version 0
+'''
+
+'''
+>>> Takes a file that contains the currency conversions for USD extracts/uses the rates to convert an amount inputted by the user. The program will then take the selected types and print them to a JSON file included with a time stamp of the conversion.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
